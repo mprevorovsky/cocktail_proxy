875fc424f7b4c424d4447d706b03b638e9dee0a9
mprevorovsky <mprevorovsky@volny.cz>
2023-09-17 11:23:35 +0200

    Added main app documentation

--- src/main/kotlin/com/example/cocktail_proxy/CocktailDbProxyApplication.kt
+++ src/main/kotlin/com/example/cocktail_proxy/CocktailDbProxyApplication.kt
@@ -4,8 +4,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 /*
-The application should function as a proxy for the Cocktail DB API (RESTful).
-API documentation can be found at https://www.thecocktaildb.com/api.php.
+The application is linked to the CocktailDB REST API.
+The CocktailDB API documentation can be found at https://www.thecocktaildb.com/api.php.
+
+The free public part of the CocktailDB API only provides GET endpoints, which all retrieve information
+either on drinks or on ingredients is some form.
+For simplicity, all JSON content returned from the remote server is mapped onto one common data structure
+(CocktailDbRecord class), and returned by the application.
+
+The application provides 2 endpoints:
+1) "/proxy/" - redirects *all* calls (all paths and queries) to the remote CocktailDB API and return the results.
+   NOTE: This single-endpoint solution simplified the proxy implementation a lot, but could become limiting if
+   future extension of the application are required.
+2) "/random_drink/" - calls the "random.php" path on the CocktailDB API to retrieve data for a random drink,
+   presented as a simple webpage (Thymeleaf template).
 */
 
 const val cocktailDbApiBaseUrl: String = "https://www.thecocktaildb.com/api/json/v1/1/"
