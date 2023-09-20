efb553ba09f0ba4747bbcf941dd81b47a7cb6b91
mprevorovsky <mprevorovsky@volny.cz>
2023-09-20 16:21:24 +0200

    Implemented drink data saving into H2 database Created "/local_db/" endpoint

---
+++ src/main/kotlin/com/example/cocktail_proxy/controller/LocalDbController.kt
@@ -0,0 +1,18 @@
+package com.example.cocktail_proxy.controller
+
+import com.example.cocktail_proxy.datasource.DrinksRepository
+import com.example.cocktail_proxy.model.DrinkJpaCompatible
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/local_db/")
+class LocalDbController(
+    private val drinkRepository: DrinksRepository
+) {
+
+    @GetMapping
+    fun getAllDrinks(): MutableIterable<DrinkJpaCompatible> = drinkRepository.findAll()
+}
+
--- src/main/kotlin/com/example/cocktail_proxy/service/ProxyService.kt
+++ src/main/kotlin/com/example/cocktail_proxy/service/ProxyService.kt
@@ -1,12 +1,15 @@
 package com.example.cocktail_proxy.service
 
 import com.example.cocktail_proxy.datasource.DataSource
+import com.example.cocktail_proxy.datasource.DrinksRepository
 import com.example.cocktail_proxy.model.CocktailDbRecord
+import com.example.cocktail_proxy.model.Drink
 import org.springframework.stereotype.Service
 
 @Service
 class ProxyService(
-    private val dataSource: DataSource
+    private val dataSource: DataSource,
+    private val drinkRepository: DrinksRepository
 ) {
     fun proxyGetRequest(
         consumedApiBaseUrl: String,
@@ -16,10 +19,30 @@ class ProxyService(
 
         val response = dataSource.proxyGetRequest(consumedApiBaseUrl, consumedApiPath, queryString)
 
-        // the following steps are just examples of introducing data transformation into the Service layer
-        response.drinks?.forEach { it.strDrink = it.strDrink?.uppercase() }
-        response.ingredients?.forEach { it.strIngredient = it.strIngredient?.uppercase() }
+        // if any drink data is returned, each *new* drink is saved to an in-memory database
+        if (!response.drinks.isNullOrEmpty()) {
+            saveDrinkData(response.drinks)
+        }
 
-        return response
+        // to demonstrates the possibilities of data transformation in the Service layer
+        // the CocktailDB response data are processed before being returned
+        // (the names of any Drinks and/or Ingredients are turned to uppercase)
+        return makeNamesUppercase(response)
+    }
+
+
+    fun makeNamesUppercase(responseToProcess: CocktailDbRecord): CocktailDbRecord {
+        responseToProcess.drinks?.forEach { it.strDrink = it.strDrink.uppercase() }
+        responseToProcess.ingredients?.forEach { it.strIngredient = it.strIngredient.uppercase() }
+
+        return responseToProcess
+    }
+
+
+    fun saveDrinkData(drinkData: Collection<Drink>) {
+        drinkData.forEach {
+            if (!drinkRepository.existsByIdDrink(it.idDrink))
+                drinkRepository.save(it.toDrinkJpaCompatible())
+        }
     }
 }
