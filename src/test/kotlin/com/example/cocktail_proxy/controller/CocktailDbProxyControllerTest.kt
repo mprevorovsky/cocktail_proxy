6e51b7f1e555dec38bca736968adfdb6bcc514a4
mprevorovsky <mprevorovsky@volny.cz>
2023-09-22 19:31:33 +0200

    Fixed tests by adding @DirtiesContext where appropriate

--- src/test/kotlin/com/example/cocktail_proxy/controller/CocktailDbProxyControllerTest.kt
+++ src/test/kotlin/com/example/cocktail_proxy/controller/CocktailDbProxyControllerTest.kt
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
+import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
 
@@ -33,6 +34,7 @@ class CocktailDbProxyControllerTest {
 
 
     @Test
+    @DirtiesContext
     fun `should return random drink data mapped to CocktailDbRecord`() {
         // when/then
         mockMvc.get("/proxy/random.php")
@@ -47,6 +49,7 @@ class CocktailDbProxyControllerTest {
 
 
     @Test
+    @DirtiesContext
     fun `should return ingredient data mapped to CocktailDbRecord`() {
         // when/then
         mockMvc.get("/proxy/search.php?i=vodka")
